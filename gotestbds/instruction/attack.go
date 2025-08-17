7597afc8eef1d0bcf89a0af2c4a0830c3b647bb0
FDUTCH <para.trabajos9999@gmail.com>
2025-08-17 17:50:23 +0200

    fix errors

--- gotestbds/instruction/attack.go
+++ gotestbds/instruction/attack.go
@@ -2,7 +2,7 @@ package instruction
 
 import (
 	"context"
-	"fmt"
+
 	"github.com/smell-of-curry/go-test-bds/gotestbds/actor"
 	"github.com/smell-of-curry/go-test-bds/gotestbds/bot"
 )
@@ -18,9 +18,6 @@ func (*Attack) Name() string {
 // Run ...
 func (a *Attack) Run(ctx context.Context, b *bot.Bot) error {
 	return execute(b, func(a *actor.Actor) error {
-		if !a.Attack() {
-			return fmt.Errorf("entity not found")
-		}
-		return nil
+		return a.Attack()
 	})
 }
--- gotestbds/instruction/attack_entity.go
+++ gotestbds/instruction/attack_entity.go
@@ -3,6 +3,7 @@ package instruction
 import (
 	"context"
 	"fmt"
+
 	"github.com/smell-of-curry/go-test-bds/gotestbds/actor"
 	"github.com/smell-of-curry/go-test-bds/gotestbds/bot"
 )
@@ -24,9 +25,6 @@ func (action *AttackEntity) Run(_ context.Context, b *bot.Bot) error {
 		if !ok {
 			return fmt.Errorf("entity does not exist")
 		}
-		if !a.AttackEntity(ent) {
-			return fmt.Errorf("failed to attack entity")
-		}
-		return nil
+		return a.AttackEntity(ent)
 	})
 }
